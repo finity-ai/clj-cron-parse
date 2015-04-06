0745e955fcf518326a31e5920b1463125af7645d
David Smith <david.smith@paper.li>
2015-04-06 18:39:24 +0200

    needed to add lein-midje plugin for travis

--- project.clj
+++ project.clj
@@ -7,4 +7,5 @@
                  [clj-time "0.9.0"]
                  [org.clojure/core.match "0.3.0-alpha4"]]
   :profiles {:dev {:dependencies [[midje "1.6.3"]]
-                   :plugins [[lein-cljfmt "0.1.10"]]}})
+                   :plugins [[lein-cljfmt "0.1.10"]
+                             [lein-midje "3.1.3"]]}})
