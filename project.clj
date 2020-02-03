3797e57c8ddfcf91d45469e20b1d3c2727b84b3a
Drew Inglis <drew@drewinglis.com>
2020-02-03 14:56:10 -0800

    Amp-ify the project, add deploy coordinates

--- project.clj
+++ project.clj
@@ -1,8 +1,13 @@
-(defproject clj-cron-parse "0.1.5-SNAPSHOT"
+(defproject amperity/clj-cron-parse "0.1.5-SNAPSHOT"
   :description "A Clojure library for using cron expressions"
-  :url "https://github.com/shmish111/clj-cron-parse"
+  :url "https://github.com/amperity/clj-cron-parse"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+
+  :deploy-branches ["master"]
+  :pedantic? :warn
+  :deploy-repositories {"releases" {:url "https://repo.clojars.org"}}
+
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [clj-time "0.9.0"]
                  [org.clojure/core.match "0.3.0-alpha4"]]
