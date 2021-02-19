e356ddbf6fa49c7ae7e27fc909207348ae76b167
Reynald Borer <reynald.borer@finity.ai>
2021-02-19 17:55:30 +0100

    Relocate project to ai.finity groupID

--- project.clj
+++ project.clj
@@ -1,11 +1,12 @@
-(defproject clj-cron-parse "0.1.5-SNAPSHOT"
+(defproject ai.finity/clj-cron-parse "0.1.5-SNAPSHOT"
   :description "A Clojure library for using cron expressions"
-  :url "https://github.com/shmish111/clj-cron-parse"
+  :url "https://github.com/finity-ai/clj-cron-parse"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [clj-time "0.9.0"]
                  [org.clojure/core.match "0.3.0-alpha4"]]
+  :pedantic? :abort
   :profiles {:dev {:dependencies [[midje "1.6.3"]]
                    :plugins [[lein-cljfmt "0.1.10"]
                              [lein-midje "3.1.3"]]}})
