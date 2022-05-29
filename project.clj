d9f1b3da3875161bd259e0dd8f1a0cabe317f6f8
Reynald Borer <361403+rborer@users.noreply.github.com>
2022-05-29 18:00:37 +0200

    Bump to Clojure 1.11.1

--- project.clj
+++ project.clj
@@ -3,9 +3,8 @@
   :url "https://github.com/finity-ai/clj-cron-parse"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.10.0"]
+  :dependencies [[org.clojure/clojure "1.11.1"]
                  [clj-time "0.15.2"]
-
                  [org.clojure/core.match "1.0.0"]]
   :pedantic? :abort
   :profiles {:dev {:dependencies [[midje "1.10.5"]]
